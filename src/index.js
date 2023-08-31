29717758ae314cc66f64f0ad12e437d8eb78e1cf
mightyprinces <happyrabbit62@gmail.com>
2023-09-01 00:38:02 +0200

    init: task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function toReadable (number) {
-  
+  let wordsTwenty = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let wordsTens = ['','','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  let res = [];
+ 
+  if (number === 0) return 'zero';
+
+  if (number%100 <= 19) {
+    res.unshift(wordsTwenty[number%100]);
+  } else {
+    res.unshift(wordsTwenty[number%10]);
+    res.unshift(wordsTens[Math.floor(number/10)%10]);
+  }
+  if (Math.floor(number/100) > 0) { res.unshift(wordsTwenty[Math.floor(number/100)] + ' hundred'); }
+
+  return res.join(' ').trim();
 }
